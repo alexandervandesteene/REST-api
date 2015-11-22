716d34004cdd178a375fff73c3ca86375fdb5364
alexandervandesteene <alexandervandesteene@mbpvanalexander.home>
2015-11-22 17:49:49 +0100

    bijna

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var bodyParser = require("body-parser"); // Body parser for fetch posted data
 var config = require("./config.json");
 var port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // Body parser use JSON data
+
 var connection = mysql.createConnection({ // Mysql Connection
   host: config.host,
   user: config.user,
@@ -19,12 +23,8 @@ http.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()); // Body parser use JSON data
-
-
 app.get('/',function(request, response){
-    response.render('index.html',{title: 'Home'})
+    response.render('index.ejs',{title: 'Home'})
 });
 
 
@@ -47,73 +47,30 @@ app.get('/book',function(req,res){
     });
 });
 
-app.post('/book',function(req,res){
-    var Bookname = req.body.bookname;
-    var Authorname = req.body.authorname;
-    var Price = req.body.price;
-    var data = {
-        "error":1,
-        "Books":""
-    };
-    if(!!Bookname && !!Authorname && !!Price){
-        connection.query("INSERT INTO book VALUES('',?,?,?)",[Bookname,Authorname,Price],function(err, rows, fields){
-            if(!!err){
-                data["Books"] = "Error Adding data";
-            }else{
-                data["error"] = 0;
-                data["Books"] = "Book Added Successfully";
-            }
-            res.json(data);
-        });
-    }else{
-        data["Books"] = "Please provide all required data (i.e : Bookname, Authorname, Price)";
-        res.json(data);
-    }
-});
-
-app.put('/book',function(req,res){
-    var Id = req.body.id;
-    var Bookname = req.body.bookname;
-    var Authorname = req.body.authorname;
-    var Price = req.body.price;
-    var data = {
-        "error":1,
-        "Books":""
-    };
-    if(!!Id && !!Bookname && !!Authorname && !!Price){
-        connection.query("UPDATE book SET BookName=?, AuthorName=?, Price=? WHERE id=?",[Bookname,Authorname,Price,Id],function(err, rows, fields){
-            if(!!err){
-                data["Books"] = "Error Updating data";
-            }else{
-                data["error"] = 0;
-                data["Books"] = "Updated Book Successfully";
-            }
-            res.json(data);
-        });
-    }else{
-        data["Books"] = "Please provide all required data (i.e : id, Bookname, Authorname, Price)";
-        res.json(data);
-    }
-});
-
-app.delete('/book',function(req,res){
-    var Id = req.body.id;
-    var data = {
-        "error":1,
-        "Books":""
-    };
-    if(!!Id){
-        connection.query("DELETE FROM book WHERE id=?",[Id],function(err, rows, fields){
-            if(!!err){
-                data["Books"] = "Error deleting data";
-            }else{
-                data["error"] = 0;
-                data["Books"] = "Delete Book Successfully";
-            }
-            res.json(data);
-        });
-    }else{
-        data["Books"] = "Please provide all required data (i.e : id )";
-        res.json(data);
-    }
+app.get('/addbook',function(req,res){
+	var Bookname = req.query.bookname;
+	var Authorname = req.query.authorname;
+	var Price = req.query.price;
+  console.log(Bookname);
+  console.log(Authorname);
+  console.log(Price);
+	var data = {
+		"error":1,
+		"Books":""
+	};
+	if(!!Bookname && !!Authorname && !!Price){
+		connection.query("INSERT INTO book VALUES("",?,?,?)",[Bookname,Authorname,Price],function(err, rows, fields){
+			if(!!err){
+				data["Books"] = "Error Adding data";
+        console.log(err);
+			}else{
+				data["error"] = 0;
+				data["Books"] = "Book Added Successfully";
+			}
+			res.json(data);
+		});
+	}else{
+		data["Books"] = "Please provide all required data (i.e : Bookname, Authorname, Price)";
+		res.json(data);
+	}
 });
